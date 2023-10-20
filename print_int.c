1676ac4a8c3f3affcccdebfb1778825ab2f96f4d
root <mhamdishaimaa@gmail.com>
2023-10-20 00:48:13 -0700

    print_int.c

--- print_int.c
+++ print_int.c
@@ -1,47 +1,69 @@
 #include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
-* print_int - function that prints an integer
-* @i: integer to print
-* Descriptions: prints digit with _putchar
-* Return: size the output text
-*/
-int print_int(va_list i)
+ * _printf - the  Printf function
+ * @format: format.
+ *
+ * Return: Printed character
+ */
+
+int _printf(const char *format, ...)
 {
-int len, powten, j, digit, n, count = 0, num;
-n = va_arg(i, int);
-if (n != 0)
+	int i, printed = 0, printed_chars = 0;
+int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
 {
-if (n < 0)
-{
-_putchar('-');
-count++;
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
-num = n;
-len = 0;
-while (num != 0)
-{
-num /= 10;
-len++;
-}
-powten = 1;
-for (j = 1; j <= len - 1; j++)
-powten *= 10;
-for (j = 1; j <= len; j++)
-{
-digit = n / powten;
-if (n < 0)
-_putchar((digit * -1) + 48);
-else
-_putchar(digit + '0');
-count++;
-n -= digit *powten;
-powten /= 10;
-}
-}
-else
-{
-_putchar('0');
-return (1);
-}
-return (count);
-}
+
